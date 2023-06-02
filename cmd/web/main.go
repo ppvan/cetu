db273116920dd2b035bc89cd8b59c85b6f2fa863
Phạm Văn Phúc <phuclaplace@gmail.com>
2023-06-02 16:31:48 +0700

    Creare info log and error log

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
 func main() {
 
-	server := http.Server{
-		Handler: routes(),
-		Addr:    ":8080",
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
 	}
 
+	server := http.Server{
+		Handler: app.routes(),
+		Addr:    *addr,
+	}
+
+	infoLog.Printf("Starting server on %s", server.Addr)
 	err := server.ListenAndServe()
-	log.Fatal(err)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 }
