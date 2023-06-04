2c8831006aa8550bf2dd9bbaebf9edfc8748d2bb
Phạm Văn Phúc <phuclaplace@gmail.com>
2023-06-04 12:38:39 +0700

    Add tailwindcss

---
+++ ui/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./html/**/*.html",
+        "./static/js/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
