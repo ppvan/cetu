eb73ba875625ee878397c3e5c0ad4fbeaff77cdd
Phạm Văn Phúc <phuclaplace@gmail.com>
2023-06-19 10:14:48 +0700

    Change db to posstgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"flag"
 	"fmt"
@@ -9,7 +10,7 @@ import (
 	"os"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"github.com/ppvan/cetu/internal/models"
 )
 
@@ -19,6 +20,12 @@ type config struct {
 	env    string
 	port   int
 	domain string
+	db     struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 }
 
 type application struct {
@@ -29,18 +36,23 @@ type application struct {
 }
 
 func main() {
-	dsn := flag.String("dsn", "cetu:cetu@/cetu?parseTime=true", "MySQL data source name")
 
 	var config config
 	flag.StringVar(&config.domain, "domain", "localhost", "Domain name")
 	flag.StringVar(&config.env, "env", "development", "Environment (development|production)")
 	flag.IntVar(&config.port, "port", 4000, "Port number")
+
+	// DB settings
+	flag.StringVar(&config.db.dsn, "dsn", os.Getenv("CETU_DB"), "Postgres data source name")
+	flag.IntVar(&config.db.maxOpenConns, "db-max-open-conns", 25, "Postgres max open connections")
+	flag.IntVar(&config.db.maxIdleConns, "db-max-idle-conns", 25, "Postgres max idle connections")
+	flag.DurationVar(&config.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "Postgres max connection idle time")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	db, err := openDB(*dsn)
+	db, err := openDB(config)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
@@ -64,19 +76,29 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", server.Addr)
-	err = server.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+	// err = server.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+	err = server.ListenAndServe()
 	if err != nil {
 		errorLog.Fatal(err)
 	}
 }
 
-func openDB(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("mysql", dsn)
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
 	if err != nil {
 		return nil, err
 	}
 
-	if err = db.Ping(); err != nil {
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+	fmt.Println(cfg.db.maxIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
 		return nil, err
 	}
 
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require github.com/julienschmidt/httprouter v1.3.0
 
-require github.com/go-sql-driver/mysql v1.7.1
-
-require github.com/justinas/alice v1.2.0
+require (
+	github.com/justinas/alice v1.2.0
+	github.com/lib/pq v1.10.9
+)
